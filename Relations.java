bcc46ad772e65e5c8d848e7d15685481c537607f
Olga Lobanova <arapova@list.ru>
2023-02-24 15:14:33 +0300

    Relations

---
+++ Relations.java
@@ -0,0 +1,13 @@
+package HWs.HW1;
+
+enum Relations {
+    PARENT,
+    CHILDREN,
+    SIBLING,
+    AUNT,
+    NIECE,
+    GRANDPARENTS,
+    GRANDCHILDREN,
+    PARENT_IN_LAW,
+    DAUGHTER_IN_LOW
+}
